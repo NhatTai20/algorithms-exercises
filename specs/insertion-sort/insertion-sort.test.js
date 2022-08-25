e8ea5790c3be29819e8dc387792211783aed0b80
NhatTai2011 <tai.vu3@tiki.vn>
2022-08-25 15:39:34 +0700

    fix insertion sort

--- specs/insertion-sort/insertion-sort.test.js
+++ specs/insertion-sort/insertion-sort.test.js
@@ -15,16 +15,11 @@
 
 function insertionSort(nums) {
   for (let i = 1; i < nums.length; i++) {
-    let numberToInsert = nums[i]; // the numberToInsert number we're looking to insert
-    let j; // the inner counter
-
-    // loop from the right to the left
+    let numberToInsert = nums[i]; 
+    let j; 
     for (j = i - 1; nums[j] > numberToInsert && j >= 0; j--) {
-      // move numbers to the right until we find where to insert
       nums[j + 1] = nums[j];
     }
-
-    // do the insertion
     nums[j + 1] = numberToInsert;
   }
   return nums;
