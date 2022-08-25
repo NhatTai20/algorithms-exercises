fd59cc4cb3e336678917c687ef3e08547e7e6577
NhatTai2011 <tai.vu3@tiki.vn>
2022-08-25 09:25:17 +0700

    optimize bubble sort

--- specs/bubble-sort/bubble-sort.test.js
+++ specs/bubble-sort/bubble-sort.test.js
@@ -12,17 +12,15 @@
 
 function bubbleSort(nums) {
   let swapped = false;
-  let iteration = 0;
   do {
     swapped = false;
-    for (let i = 0; i < nums.length; i++) {
+    for (let i = 0; i < nums.length - 1; i++) {
       if (nums[i] > nums[i + 1]) {
         const temp = nums[i];
         nums[i] = nums[i + 1];
         nums[i + 1] = temp;
         swapped = true;
       }
-      iteration++;
     }
   } while (swapped);
   return nums;
